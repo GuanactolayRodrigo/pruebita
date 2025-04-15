eee81a654ab1a9e51dd75a104695d394d984e1b9
Cari UwU <luckpruevas@gmail.com>
2025-04-15 13:09:17 -0300

    agregar un archivo javascript

---
+++ ej1.js
@@ -0,0 +1,10 @@
+/*1.Crear una variable denominada integrantes y asignarle la cadena de texto con los
+nombres de los integrantes del grupo.
+Mostrar por consola el siguiente texto:
+Somos (el valor de la variable) y estamos aprendiendo javascript*/
+
+let grupo11 = "Cari, Rodrigo, Victor, Matias";
+
+let integrantes = nombres => console.log("Somos " + nombres + " aprendiendo javascript");
+
+integrantes(grupo11);
