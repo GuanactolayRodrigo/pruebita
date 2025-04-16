ef36380b72dfc174ec12d9f0dd325df3d6989a0e
Cari UwU <luckpruevas@gmail.com>
2025-04-15 21:06:41 -0300

    agregar puntos

---
+++ ej13.js
@@ -0,0 +1,12 @@
+/*13. Escribir una función llamada mostrarArreglo que reciba un arreglo numérico como 
+parámetro e imprima cada elemento en una línea a parte.*/
+
+let arregloNumerico = [1, 2, 3, 4, 5];
+
+function mostrarArreglo(arreglos){   
+    for (let i = 0; i < arreglos.length; i++) {
+        console.log(arreglos[i]);
+    }
+}
+
+mostrarArreglo(arregloNumerico);
