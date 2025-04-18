8b10e4ea1c94aa24a6a7c036b9152021660e96f7
Cari UwU <luckpruevas@gmail.com>
2025-04-17 21:45:36 -0300

    ejercicio 23 listo

--- ej23.js
+++ ej23.js
@@ -1,15 +1,44 @@
 
-let radio = document.getElementsByName("lenguaje");
+let radio = document.querySelectorAll(".inp");
 let res = document.getElementById("resultado");
 let valorRadio = "";
 
 
-// Agregar listener a cada radio button
+radio.forEach(function(item) {
+  item.addEventListener("change", function() {
+    if (item.checked) {
+      valorRadio = item.value;
+      res.innerHTML = "El lenguaje seleccionado es: " + valorRadio;
+    }
+  });
+});
+
+
+
+/*// Agregar listener a cada radio button
 for (let i = 0; i < radio.length; i++) {
-  radio[i].addEventListener("change", function() {
+
+  console.log("Prueba");
+  radio[i].addEventListener("change", function() { //click,change,input
     if (radio[i].checked) {
       valorRadio = radio[i].value;
       res.innerHTML = "El lenguaje seleccionado es: " + valorRadio;
     }
   });
-}
+}*/
+
+// radio[0].addEventListener("change", function() { //click,change,input
+//   if (radio[0].checked) {
+//     valorRadio = radio[0].value;
+//     res.innerHTML = "El lenguaje seleccionado es: " + valorRadio;
+//   }
+// });
+
+
+//   radio[1].addEventListener("change", function() { //click,change,input
+//     if (radio[1].checked) {
+//       valorRadio = radio[1].value;
+//       res.innerHTML = "El lenguaje seleccionado es: " + valorRadio;
+//     }
+//   });
+  
