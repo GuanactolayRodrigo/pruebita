dd81e1d26e01936cd212a11644150fe7874ce00e
GuanactolayRodrigo <42583662@fi.edu.unju.ar>
2025-04-18 22:01:57 -0300

    ejercicio terminado

---
+++ ej4.js
@@ -0,0 +1,6 @@
+const lado = 8;
+let area = lado * 2;
+let perimetro = lado * 4;
+console.log("Lado del cuadrado: " + lado);
+console.log("El area del cuadrado es: " + area);
+console.log("El perimetro del cuadrado es: " + perimetro);
